4484b06865d56bca22fe2fe42e1c36b68981b8ec
Jad Menkara <Jadmenkara@gmail.com>
2024-08-05 23:54:38 -0400

    fixed some stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,29 @@
-<div class="bg-gradient-to-r from-purple-900 via-gray-900 to-pink-500 p-3">
-    <div>
-        <h1>
+<div class="bg-gradient-to-r from-purple-900 via-gray-900 to-pink-500 p-4 opacity-85 flex items-center sticky" id="navbar" >
+    <div label ="Name  mb-2 mt-4">
+        <h1 class ="font-semibold text-2xl">
             Jad Menkara
         </h1>
-        <h2>
+
+        <br>
+
+        <h2 class ="font-medium text-sm">
             Aspiring Biomedical Engineer
         </h2>
-        <h3>
+        <h3 class ="font-light text-xs">
             Web Developer
         </h3>
 
     </div>
 
-
+    <div id="nav-button-group" class="ml-auto">
+        <button class="nav-button">About Me</button>
+        <button class="nav-button">Resume</button>
+        <button class="nav-button">Projects</button>
+        <button class="nav-button">Contacts</button>
+    </div>
 
 </div>
+<br>
+<br>
+<br>
+<br>
