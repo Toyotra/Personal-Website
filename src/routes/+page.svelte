c0041f49b05af5eb230ae3d0f267d8e4a96d08ed
Jad Menkara <Jadmenkara@gmail.com>
2024-08-05 22:40:35 -0400

    I added some stuff

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<div class="bg-gradient-to-r from-purple-900 via-gray-900 to-pink-500 p-3">
+    <div>
+        <h1>
+            Jad Menkara
+        </h1>
+        <h2>
+            Aspiring Biomedical Engineer
+        </h2>
+        <h3>
+            Web Developer
+        </h3>
+
+    </div>
+
+
+
+</div>
