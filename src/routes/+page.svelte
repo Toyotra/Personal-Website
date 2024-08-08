cc2cd7fec6d4bf9f2aedff458eedac6ca011d239
Jad Menkara <Jadmenkara@gmail.com>
2024-08-08 14:40:37 -0400

    added a bunch of stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,6 @@
-<div class="bg-gradient-to-r from-purple-900 via-gray-900 to-pink-500 p-4 opacity-85 flex items-center sticky top-0" id="navbar" >
+
+
+<div class="bg-gradient-to-r from-purple-900/60 via-gray-900/60 to-pink-500/60 p-4  flex items-center sticky top-0 z-[1]" id="navbar" >
     <div label ="Name  mb-2 mt-4">
         <h1 class ="font-semibold text-2xl">
             Jad Menkara
@@ -74,7 +76,7 @@
 <br>
 
 <div class="ml-auto mr-auto flex justify-center">
-    <h1 class="group text-center font-semibold text-2xl transition duration-200 inline-block">
+    <h1 class="group text-center font-semibold text-3xl transition duration-200 inline-block">
         Photo Gallery!
         <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-sky-600"></span>
     </h1>
@@ -123,21 +125,145 @@
 <br><br>
 
 <div class= "flex justify-center">
-    <div class="bg-sky-950 inline-block rounded-3xl border-4 transistion ease-in-out duration-300 hover:border-8 hover:opacity-50 hover:p-2">
+    <div class="project-container">
+            <div class="flex justify-center m-4">
+                <div class="inline-block">
+                    <a class = "group font-semibold text-2xl transistion duration:300 hover:text-blue-400" href="https://github.com/Toyotra/Personal-Website">
+                        Portfolio Website
+                        <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-sky-600"></span>
+                    </a>
+                    
+                </div>
+            </div>
+
+    
+        <img class="project-photo" src = "assets/Images/project photos/portfolio site.png">
+        <p class="pop-up-text">
+            Portfolio website created in Svelte, Tailwind, and Javascript. The website showcases my various achievements
+            such as projects, awards, photos, and my resume.
+        </p>
+    </div>
+
+    <div class="project-container">
+
         <div class="flex justify-center m-4">
             <div class="inline-block">
-                <a class = "group font-semibold text-2xl transistion duration:300 hover:text-blue-400" href="https://github.com/Toyotra/Personal-Website">
-                    Portfolio Website
+                <a class = "group font-semibold text-2xl transistion duration:300 hover:text-blue-400" href="https://www.linkedin.com/posts/dorothy-zheng07_this-weekend-jad-menkara-and-i-competed-in-activity-7177809048770150400-L_4d?utm_source=share&utm_medium=member_desktop">
+                    Transhumeral Bionic Prosthesis
                     <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-sky-600"></span>
                 </a>
                 
             </div>
         </div>
 
-    
-        <img class="project-photo" src = "assets/Images/project photos/portfolio site.png">
+
+    <img class="project-photo" src = "assets/Images/project photos/prosthesis.jpeg">
+    <p class="pop-up-text">
+        Me and my friend created a novel bionic prosthesis for below the shoulder-elbow inclusive amputees.
+        I designed a custom PCB to house the various electronics such as a Raspberry Pi Pico microcontroller, a high-current buck converter,
+        and peripheral leads for external sensors. I also CADed the entire design and 3D printed it on my home printer.
+    </p>
     </div>
 
+    
 
 
 </div>
+
+
+
+<div class="flex justify-center">
+    <div class="project-container">
+
+        <div class="flex justify-center m-4">
+            <div class="inline-block">
+                <a class = "group font-semibold text-2xl transistion duration:300 hover:text-blue-400" href="https://projectboard.world/ysc/project/novel-thermoelectric-generator-for-automobile-exhaust-waste-heat-recovery">
+                    Automobile Exhaust Thermoelectric Generator
+                    <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-sky-600"></span>
+                </a>
+                
+            </div>
+        </div>
+
+
+    <img class="project-photo" src = "assets/Images/project photos/thermo.png">
+    <p class="pop-up-text">
+        Presented here is a novel thermoelectric generator based waste heat recovery system aimed at improving overall combustion engine efficiency by reducing energy losses through heat through the use of TEG modules. By utilizing waste heat from an automobile exhaust and maintaining a temperature differential, sustained output power is possible.
+    </p>
+    </div>
+
+
+    <div class="project-container">
+
+        <div class="flex justify-center m-4">
+            <div class="inline-block">
+                <a class = "group font-semibold text-2xl transistion duration:300 hover:text-blue-400" href="https://github.com/Toyotra/Nerf-Gun-with-Computer-Vision">
+                    Auto-Tracking Nerf Gun w/Computer Vision
+                    <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-sky-600"></span>
+                </a>
+                
+            </div>
+        </div>
+
+
+    <img class="project-photo" src = "assets/Images/project photos/nerf gun.jpg">
+    <p class="pop-up-text">
+        I created this project for our local Hack Club chapter. 
+        It uses YOLOv8, Pyfirmata Arduino communication, 3D printing, and other engineering concepts. By tracking faces via computer-vision and a mounted camera, and rotating a nerf-gun accordingly via servo motors, 
+        we are capable of creating a device which can target faces purely through automated aiming!
+    </p>
+    </div>
+
+
+</div>
+
+<br>
+<br>
+<br>
+<br>
+
+<div class="ml-auto mr-auto flex justify-center">
+    <a class="group text-center font-semibold text-3xl transition duration-200 inline-block hover:text-blue-900" href="https://github.com/Toyotra/Jad-Menkara-Resume">
+        Resume
+        <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-sky-600"></span>
+    </a>
+</div>
+
+
+<br>
+
+
+<div class="flex justify-center">
+    <embed src = "assets/Jad Menkara Resume.pdf#view=FitH&navpanes=0" width="1200px" height="1600px"  />
+</div>
+
+
+
+<br>
+<br>
+<br>
+<br>
+
+
+
+<div class="ml-auto mr-auto flex justify-center">
+    <h1 class="group text-center font-semibold text-2xl transition duration-200 inline-block ">
+        Check out my socials!
+        <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-sky-600"></span>
+    </h1>
+</div>
+
+<br>
+<div class="flex justify-center">
+    <a  href = "https://github.com/Toyotra" class="m-2 hover:opacity-50 ease-in-out transition duration-500">
+        
+        <i class="fa-brands fa-github fa-2x"></i>
+    </a>
+    <a  href = "https://www.linkedin.com/in/jad-menkara-1593942aa/" class="m-2 hover:opacity-50 ease-in-out transition duration-500">
+        <i class="fa-brands fa-linkedin fa-2x"></i>
+    </a>
+    <a  href = "mailto:jadmenkara@gmail.com" class="m-2 hover:opacity-50 ease-in-out transition duration-500">
+        <i class="fa-solid fa-envelope fa-2x"></i>
+    </a>
+</div>
+<br>
