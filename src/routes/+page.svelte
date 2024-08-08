8c67e30cf04e00cc7726771b05b1528c120aaab0
Jad Menkara <Jadmenkara@gmail.com>
2024-08-08 01:55:45 -0400

    Made it a lot more dope

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
-<div class="bg-gradient-to-r from-purple-900 via-gray-900 to-pink-500 p-4 opacity-85 flex items-center sticky" id="navbar" >
+<div class="bg-gradient-to-r from-purple-900 via-gray-900 to-pink-500 p-4 opacity-85 flex items-center sticky top-0" id="navbar" >
     <div label ="Name  mb-2 mt-4">
         <h1 class ="font-semibold text-2xl">
             Jad Menkara
@@ -27,3 +27,117 @@
 <br>
 <br>
 <br>
+
+<div class="flex justify-evenly items-center">
+    <img id = "portfolio-shake" src = "assets/Images/portfolio photo.svg" alt = "portfolio photo">
+    <div class="w-1/2">
+        <div class="w-1/2">
+            <div class="inline-block">
+                <h2 class = "group font-semibold text-3xl transistion duration:300">
+                    About Me
+                    <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-sky-600"></span>
+                </h2>
+                
+            </div>
+            
+            <br>
+            <br>
+            <br>
+        </div>
+        <p class="font-light">Hey there! My name is Jad Menkara, and I am an incoming 11th grade student from Vincent Massey Secondary School, Windsor, Ontario.
+            I have a passion for engineering and design and have created various projects in the past like my 
+            <a class= " text-blue-700 font-semibold transition duration-300 hover:text-blue-900"href="https://www.linkedin.com/posts/dorothy-zheng07_this-weekend-jad-menkara-and-i-competed-in-activity-7177809048770150400-L_4d?utm_source=share&utm_medium=member_desktop">
+                Transhumeral Bionic Prosthesis
+            </a>, and my 
+            <a href = "https://github.com/Toyotra/Nerf-Gun-with-Computer-Vision" class= " text-blue-700 font-semibold transition duration-300 hover:text-blue-900">
+                Nerf Gun w/Computer Vision
+            </a>           
+            I created in association with our local Hack Club. I participate in various after school clubs like Research Club, Hack Club, Computer Science Club,
+            and Chess Club. I have played a leading role in our local Hack Club chapter and was promoted to President this May. I have many hobbies and in my
+            free time I enjoy playing guitar, playing Tetris, and weightlifting. In the future, I hope to become a sucessful biomedical engineer and create
+            some dope robots. &#128526;
+        </p>
+    </div>
+</div>
+
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+<div class="ml-auto mr-auto flex justify-center">
+    <h1 class="group text-center font-semibold text-2xl transition duration-200 inline-block">
+        Photo Gallery!
+        <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-sky-600"></span>
+    </h1>
+</div>
+
+<br>
+<br>
+<br>
+
+<div class="flex justify-center">
+    <img src="assets/Images/website photos/photo1.jpg" class="photo-gallery">
+    <img src="assets/Images/website photos/photo2.jpg" class="photo-gallery">
+    <img src="assets/Images/website photos/photo3.jpg" class="photo-gallery">
+    <img src="assets/Images/website photos/photo4.jpg" class="photo-gallery">
+    
+
+</div>
+
+<div class="flex justify-center">
+    <img src="assets/Images/website photos/photo5.jpg" class="photo-gallery">
+    <img src="assets/Images/website photos/photo6.jpg" class="photo-gallery">
+    <img src="assets/Images/website photos/photo7.jpg" class="photo-gallery">
+    <img src="assets/Images/website photos/photo8.jpg" class="photo-gallery">
+    
+
+</div>
+
+
+<br>
+
+<br>
+<br>
+<br>
+<br>
+
+<div class="flex justify-center">
+    <div class="inline-block">
+        <h2 class = "group font-semibold text-3xl transistion duration:300">
+            Projects
+            <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-sky-600"></span>
+        </h2>
+        
+    </div>
+</div>
+
+<br><br>
+
+<div class= "flex justify-center">
+    <div class="bg-sky-950 inline-block rounded-3xl border-4 transistion ease-in-out duration-300 hover:border-8 hover:opacity-50 hover:p-2">
+        <div class="flex justify-center m-4">
+            <div class="inline-block">
+                <a class = "group font-semibold text-2xl transistion duration:300 hover:text-blue-400" href="https://github.com/Toyotra/Personal-Website">
+                    Portfolio Website
+                    <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-sky-600"></span>
+                </a>
+                
+            </div>
+        </div>
+
+    
+        <img class="project-photo" src = "assets/Images/project photos/portfolio site.png">
+    </div>
+
+
+
+</div>
